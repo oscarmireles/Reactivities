c00239f3a380a9cd0c6b3afd04d7f62de45a9fe8
Oscar Mireles <oscaroner@gmail.com>
2024-11-07 01:15:10 -0600

    Add state management with MobX (section 7)

--- client-app/src/main.tsx
+++ client-app/src/main.tsx
@@ -3,9 +3,12 @@ import { createRoot } from 'react-dom/client'
 import 'semantic-ui-css/semantic.min.css'
 import App from './app/layout/App'
 import './app/layout/styles.css'
+import { store, StoreContext } from './app/stores/store'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App/>
-  </StrictMode>,
+    <StoreContext.Provider value={store}>
+      <App />
+    </StoreContext.Provider>
+  </StrictMode> 
 )
