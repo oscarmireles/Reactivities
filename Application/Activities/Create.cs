91d47d3692848f7b027d89f073a72476376bc561
Oscar Mireles <oscaroner@gmail.com>
2024-12-03 22:05:09 -0600

    Improved forms with Formik (section 11)

--- Application/Activities/Create.cs
+++ Application/Activities/Create.cs
@@ -34,7 +34,7 @@ public class Create
         {
             _context.Activities.Add(request.Activity);
 
-            var result = await _context.SaveChangesAsync() > 1;
+            var result = await _context.SaveChangesAsync() > 0;
 
             if(!result) return Result<Unit>.Failure("Failed to create activity");
 
