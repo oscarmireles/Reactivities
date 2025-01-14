cd842ce607abc8be3ace013b51b7119dc2b8fbe5
Oscar Mireles <oscaroner@gmail.com>
2025-01-13 22:41:31 -0600

    Added client side login and registration (section 13)

--- client-app/src/features/activities/home/HomePage.tsx
+++ client-app/src/features/activities/home/HomePage.tsx
@@ -1,17 +1,34 @@
 import { Link } from "react-router-dom";
 import { Button, Container, Header, Image, Segment } from "semantic-ui-react";
+import { useStore } from "../../../app/stores/store";
+import LoginForm from "../users/LoginForm";
+import RegisterForm from "../users/RegisterForm";
 
 export default function HomePage() {
+    const { userStore, modalStore } = useStore();
     return (
         <Segment inverted textAlign='center' vertical className='masthead'>
             <Container text>
                 <Header as='h1' inverted>
                     <Image size='massive' src='/assets/logo.png' alt='logo' style={{ marginTop: 12 }} />
                 </Header>
-                <Header as='h2' inverted content='Welcome to Reactivities' />
-                <Button as={Link} to='/activities' size='huge'>
-                    Take me to the activities
-                </Button>
+                {userStore.isLoggedIn ? (
+                    <>
+                        <Header as='h2' inverted content='Welcome to Reactivities' />
+                        <Button as={Link} to='/activities' size='huge'>
+                            Go to Activities!
+                        </Button>
+                    </>
+                ) : (
+                    <>
+                        <Button onClick={() => modalStore.openModal(<LoginForm />)} size='huge'>
+                            Login!
+                        </Button>
+                        <Button onClick={() => modalStore.openModal(<RegisterForm />)} size='huge'>
+                            Register
+                        </Button>
+                    </>
+                )}
             </Container>
 
         </Segment>
